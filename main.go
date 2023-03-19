ab6bdb90ff4ff502881b7e6afc89f0bfec2dcc7d
Benjamin A. Stockwell <bstockwell@semafor.com>
2023-03-19 09:15:47 -0500

    working g-counter

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"log"
-	"sync"
 	"time"
-
-	"github.com/google/uuid"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -17,151 +15,65 @@ type MessageBody struct {
 	MsgID int    `json:"msg_id"`
 }
 
-type EchoMessageBody struct {
+type AddMessageBody struct {
 	MessageBody
-	Echo any `json:"echo"`
-}
-
-type TopologyMessageBody struct {
-	MessageBody
-	Topology map[string][]string `json:"topology,omitempty"`
-}
-
-type GenerateMessageBody struct {
-	MessageBody
-	ID string `json:"id"`
-}
-
-type BroadcastMessageBody struct {
-	MessageBody
-	Message int `json:"message"`
+	Delta int `json:"delta,omitempty"`
 }
 
 type ReadMessageBody struct {
 	MessageBody
-	Messages []int `json:"messages"`
-}
-
-type BroadcastOkMessageBody struct {
-	MessageBody
+	Value int `json:"value"`
 }
 
 func main() {
 	n := maelstrom.NewNode()
+	kv := maelstrom.NewSeqKV(n)
 
-	neighbors := map[string]any{}
-
-	messages := map[int]any{}
-	messagesMu := sync.Mutex{}
-
-	n.Handle("echo", func(msg maelstrom.Message) error {
-		var body EchoMessageBody
+	n.Handle("add", func(msg maelstrom.Message) error {
+		var body AddMessageBody
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		body.Type = "echo_ok"
+		for {
+			breakLoop := false
 
-		return n.Reply(msg, body)
-	})
+			readVal, err := kv.ReadInt(context.Background(), "value")
 
-	n.Handle("generate", func(msg maelstrom.Message) error {
-		var body GenerateMessageBody
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		id := uuid.New()
-
-		body.Type = "generate_ok"
-		body.ID = id.String()
-
-		return n.Reply(msg, body)
-	})
-
-	n.Handle("topology", func(msg maelstrom.Message) error {
-		var body TopologyMessageBody
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		for _, n := range body.Topology[n.ID()] {
-			neighbors[n] = struct{}{}
-		}
-
-		body.Topology = map[string][]string{}
-		body.Type = "topology_ok"
-
-		return n.Reply(msg, body)
-	})
-
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body BroadcastMessageBody
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		msgCopy := body.Message
-
-		messagesMu.Lock()
-		_, seen := messages[body.Message]
-		messages[body.Message] = struct{}{}
-		messagesMu.Unlock()
-
-		err := n.Reply(msg, MessageBody{
-			Type: "broadcast_ok",
-		})
-
-		rebroadcastMsg := BroadcastMessageBody{
-			MessageBody: MessageBody{
-				Type: "broadcast",
-			},
-			Message: msgCopy,
-		}
-
-		if !seen {
-			neighborsToGossip := map[string]any{}
-			mu := sync.Mutex{}
-
-			for n := range neighbors {
-				if n != msg.Src {
-					neighborsToGossip[n] = neighbors[n]
-				}
-			}
-
-			for {
-				for node := range neighborsToGossip {
-					n.RPC(node, rebroadcastMsg, func(m maelstrom.Message) error {
-						var body MessageBody
-						if err := json.Unmarshal(m.Body, &body); err != nil {
-							return err
-						}
-
-						if body.Type == "broadcast_ok" {
-							mu.Lock()
-							delete(neighborsToGossip, node)
-							mu.Unlock()
-						} else {
-							return errors.New("invalid response to broadcast message")
-						}
-
-						return nil
-					})
-				}
-
-				time.Sleep(time.Millisecond * 5000)
-
-				if len(neighborsToGossip) == 0 {
+			if err != nil {
+				switch maelstrom.ErrorCode(err) {
+				case maelstrom.KeyDoesNotExist:
 					break
+				default:
+					return err
 				}
+			}
+
+			newVal := readVal + body.Delta
+
+			err = kv.CompareAndSwap(context.Background(), "value", readVal, newVal, true)
+
+			if err == nil {
+				break
+			}
+
+			switch maelstrom.ErrorCode(err) {
+			case maelstrom.PreconditionFailed:
+				time.Sleep(time.Millisecond * 200)
+				continue
+			default:
+				breakLoop = true
+			}
+
+			if breakLoop {
+				break
 			}
 		}
 
-		return err
-	})
+		body.Delta = 0
+		body.Type = "add_ok"
 
-	n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
-		return nil
+		return n.Reply(msg, body)
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
@@ -170,13 +82,19 @@ func main() {
 			return err
 		}
 
-		body.Type = "read_ok"
+		readVal, err := kv.ReadInt(context.Background(), "value")
 
-		messagesMu.Lock()
-		for msg := range messages {
-			body.Messages = append(body.Messages, msg)
+		if err != nil {
+			switch maelstrom.ErrorCode(err) {
+			case maelstrom.KeyDoesNotExist:
+				break
+			default:
+				return fmt.Errorf("error reading from kv: %v", err)
+			}
 		}
-		messagesMu.Unlock()
+
+		body.Value = readVal
+		body.Type = "read_ok"
 
 		return n.Reply(msg, body)
 	})
